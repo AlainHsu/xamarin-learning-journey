12f914741c05236d04fe24e1070c5298eadd525f
Alain Hsu <alain.hsu@outlook.com>
2023-06-13 15:16:02 +0800

    feat: craete local database sample project

--- 2- Xamarin Forms Tutorials/LocalDatabaseTutorial/LocalDatabaseTutorial/LocalDatabaseTutorial/App.xaml.cs
+++ 2- Xamarin Forms Tutorials/LocalDatabaseTutorial/LocalDatabaseTutorial/LocalDatabaseTutorial/App.xaml.cs
@@ -1,29 +1,45 @@
 ﻿using System;
+using System.IO;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace LocalDatabaseTutorial
 {
     public partial class App : Application
     {
-        public App ()
+        static Database database;
+
+        public static Database Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "people.db3"));
+                }
+                return database;
+            }
+        }
+
+        public App()
         {
             InitializeComponent();
 
             MainPage = new MainPage();
         }
 
-        protected override void OnStart ()
+        protected override void OnStart()
         {
+            // Handle when your app starts
         }
 
-        protected override void OnSleep ()
+        protected override void OnSleep()
         {
+            // Handle when your app sleeps
         }
 
-        protected override void OnResume ()
+        protected override void OnResume()
         {
+            // Handle when your app resumes
         }
     }
 }
-
